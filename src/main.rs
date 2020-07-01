b4cf4d5d2a416310a9b353d97da2f8015dbca2d8
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2020-07-01 17:29:40 +0000

    Added buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ mod my_graphics;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
+
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+
+use vulkano::buffer::BufferUsage;
+use vulkano::buffer::CpuAccessibleBuffer;
+
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+
 use vulkano_win::VkSurfaceBuild;
 
 use winit::{
@@ -22,12 +32,53 @@ fn main() {
             .expect("failed to create Vulkan instance")
     };
 
-    let _physical = PhysicalDevice::enumerate(&instance)
+    let physical = PhysicalDevice::enumerate(&instance)
         .next()
         .expect("no device available");
 
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+                    [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+    let queue = queues.next().unwrap();
+
+    /*
+    BUFFER
+     */
+    struct MyStruct {
+        a: u32,
+        b: bool,
+    }
+
+    let data = MyStruct { a: 5, b: true };
+    let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, data)
+        .expect("Failed to create buffer");
+
+    let mut content = buffer.write().unwrap();
+
+    println!("buffer content a{}",content.a);
+    println!("buffer content b{}",content.b);
+
+    content.a *= 2;
+    content.b = false;
+
+    println!("buffer content a{}",content.a);
+    println!("buffer content b{}",content.b);
+    // BUFFER
+
+    /*
+    COMMAND BUFFER
+     */
+
+    // COMMAND BUFFER
+
+
+
     let events_loop = EventLoop::new();
-    let _surface = WindowBuilder::new()
+    let surface = WindowBuilder::new()
         .with_title("My First Window")
         .with_resizable(false )
         .build_vk_surface(&events_loop, instance.clone())
@@ -41,4 +92,5 @@ fn main() {
             _ => ()
         }
     });
+
 }
