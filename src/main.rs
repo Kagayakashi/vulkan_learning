bf4120527aa042e961c328210559b775a532d774
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2020-07-06 03:33:32 +0000

    Little text change

--- src/main.rs
+++ src/main.rs
@@ -2,114 +2,136 @@ mod my_graphics;
 
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
-use vulkano::instance::PhysicalDevice;
-
-use vulkano::device::Device;
-use vulkano::device::DeviceExtensions;
-use vulkano::device::Features;
-
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
-
-use vulkano::command_buffer::{AutoCommandBufferBuilder, AutoCommandBuffer};
-use vulkano::command_buffer::CommandBuffer;
 
 use vulkano_win::VkSurfaceBuild;
 
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder},
+    window::{
+        Window,
+    },
 };
+use vulkano_win::CreationError::WindowCreationError;
 
-fn main() {
-    println!("Hello, world!");
+struct HelloTriangleApplication {
+    event_loop: EventLoop<()>,
+    window: Window,
+}
 
-    my_graphics::my_render::my_vulkan::test();
+impl HelloTriangleApplication {
+    pub fn run(&mut self){
+        self.init_window();
+        self.init_vulkan();
+        self.init_loop();
 
-    let instance = {
-        let extensions = vulkano_win::required_extensions();
-        Instance::new(None, &extensions, None)
-            .expect("failed to create Vulkan instance")
-    };
-
-    let physical = PhysicalDevice::enumerate(&instance)
-        .next()
-        .expect("no device available");
-
-    let queue_family = physical.queue_families()
-        .find(|&q| q.supports_graphics())
-        .expect("couldn't find a graphical queue family");
-    let (device, mut queues) = {
-        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
-                    [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
-    };
-    let queue = queues.next().unwrap();
-
-    /*
-    BUFFER
-     */
-    struct MyStruct {
-        a: u32,
-        b: bool,
+        println!("Application started successful!");
     }
 
-    let data = MyStruct { a: 5, b: true };
-    let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, data)
-        .expect("Failed to create buffer");
+    fn init_window(&mut self) {
+        println!("Window initialization...");
+        /*
+        let event_loop = EventLoop::new();
+        let _window = WindowBuilder::new()
+            .build(&event_loop)
+            .unwrap();
+        self.event_loop = event_loop;
+        */
+        self.event_loop = EventLoop::new();
+        self.window = Window::new(&self.event_loop)
+            .unwrap();
+    }
 
-    let mut content = buffer.write().unwrap();
+    fn init_vulkan(&mut self) {
+        println!("Vulkan initialization...");
+    }
 
-    println!("buffer content a{}",content.a);
-    println!("buffer content b{}",content.b);
+    fn init_loop(&mut self){
+        println!("Loop initialization...");
 
-    content.a *= 2;
-    content.b = false;
+        self.event_loop.run(move |event, _, control_flow| {
+            // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+            // dispatched any events. This is ideal for games and similar applications.
+            *control_flow = ControlFlow::Poll;
 
-    println!("buffer content a{}",content.a);
-    println!("buffer content b{}",content.b);
-    // BUFFER
+            // ControlFlow::Wait pauses the event loop if no events are available to process.
+            // This is ideal for non-game applications that only update in response to user
+            // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+            //*control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    println!("The close button was pressed; stopping");
+                    *control_flow = ControlFlow::Exit
+                },
+                Event::MainEventsCleared => {
+                    // Application update code.
+                    // Queue a RedrawRequested event.
+                    self.window.request_redraw();
+                },
+                Event::RedrawRequested(_) => {
+                    // Redraw the application.
+                },
+                _ => ()
+            }
+        });
+    }
+}
+
+fn main() {
+
+    // Include my_graphics/my_render/my_vulkan/mod.rs
+    my_graphics::my_render::my_vulkan::test();
+    //--------------------------------------------------------------------------------------------//
+    //let app = HelloTriangleApplication::initialize();
+    let mut app = HelloTriangleApplication;
+    app.run();
 
     /*
-    COMMAND BUFFER
-     */
-    let source_content = 0 .. 64;
-    let source = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, source_content)
-        .expect("Failed to create Source buffer");
-
-    let dest_content = 0 .. 64;
-    let dest = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, dest_content)
-        .expect("Failed to create Source buffer");
-
-    let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .build(&event_loop)
         .unwrap();
-    builder.copy_buffer(source.clone(), dest.clone())
-        .unwrap();
+    event_loop.run(move |event, _, control_flow| {
+        // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+        // dispatched any events. This is ideal for games and similar applications.
+        *control_flow = ControlFlow::Poll;
 
-    let command_buffer = builder.build()
-        .unwrap();
-    // Sumbission and Synchronization
-    let finished = command_buffer.execute(queue.clone())
-        .unwrap();
-    // TODO
-    // COMMAND BUFFER
+        // ControlFlow::Wait pauses the event loop if no events are available to process.
+        // This is ideal for non-game applications that only update in response to user
+        // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+        *control_flow = ControlFlow::Wait;
 
-
-
-    let events_loop = EventLoop::new();
-    let surface = WindowBuilder::new()
-        .with_title("My First Window")
-        .with_resizable(false )
-        .build_vk_surface(&events_loop, instance.clone())
-        .unwrap();
-
-    events_loop.run(|event, _, control_flow| {
         match event {
-            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-                *control_flow = ControlFlow::Exit;
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                *control_flow = ControlFlow::Exit
+            },
+            Event::MainEventsCleared => {
+                // Application update code.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw, in
+                // applications which do not always need to. Applications that redraw continuously
+                // can just render here instead.
+                _window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
             },
             _ => ()
         }
     });
-
+     */
 }
